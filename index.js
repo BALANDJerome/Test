a0fe63238c556856595f7b2af7bc87db12ede9a4
BALAND Jérome <baland.jerome@gmail.com>
2025-03-05 20:57:18 +0100

    js

---
+++ index.js
@@ -0,0 +1,14 @@
+const div = document.querySelector("div");
+let rotat = [0, 1];
+
+const reset = (a, b) => {
+  div.style.transform = "rotate(" + a + "deg) scale(" + b + ")";
+  console.log(rotat);
+};
+
+div.addEventListener("click", (e) => {
+  if (rotat[1] === 1) {
+    reset((rotat[0] += 90), (rotat[1] = -1));
+  } else reset((rotat[0] += 90), (rotat[1] = 1));
+  // rotat[1]=-1;
+});
