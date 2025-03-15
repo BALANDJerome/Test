6c8843613b982ab375b9840657f5675501ef581e
BALAND Jérome <baland.jerome@gmail.com>
2025-03-15 20:20:14 +0100

    debut test creatDiv

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const divs = document.querySelectorAll("div");
+const divs = document.querySelectorAll(".zoomDiv > div");
+const crea = document.querySelector(".creatDiv");
 
 divs.forEach((div) => {
   const removeClass = () => {
@@ -15,10 +16,26 @@ divs.forEach((div) => {
       removeClass(div);
       div.style.transform = "scale(1.8)";
       div.style.cursor = "zoom-out";
-      // check();
       div.classList.add("zoom");
     } else {
       removeClass(div);
     }
   });
 });
+
+let ind = 0;
+const createDiv = () => {
+  console.log(ind);
+  if (ind < 10) {
+    crea.innerHTML += `<div></div>`;
+    ind += 1;
+    console.log(ind);
+    setTimeout(() => {
+      createDiv();
+    }, 1000);
+  } else {
+    ind = 0;
+    console.log("stop");
+  }
+};
+createDiv();
