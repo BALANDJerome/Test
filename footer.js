8ccc4ddda0456043bb95fcd0044ce8e2c6730630
BALAND Jérome <baland.jerome@gmail.com>
2025-04-04 20:02:06 +0200

    ajout footer et header avec JS

---
+++ footer.js
@@ -0,0 +1,5 @@
+const headerContent = `<p>Je suis un haut de page =)</p>`;
+document.getElementById("Header").innerHTML = headerContent;
+
+const footerContent = `<p>je suis un pied de page =)</p>`;
+document.getElementById("Footer").innerHTML = footerContent;
